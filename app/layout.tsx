e15d81e36bd1a673dfd1eaa2b3b9a73059304fbd
DylDev <dylanb92010@gmail.com>
2026-07-31 06:45:02 -0400

    Make it run for testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from "@/components/ui/sonner"
 import { Instrument_Sans } from 'next/font/google';
 import { ThemeProvider } from "next-themes"
-import { usePathname } from 'next/navigation';
 import "./globals.css"
 
 import { Footer } from '@/components/footer';
@@ -13,8 +12,6 @@ const instrumentSans = Instrument_Sans({
 });
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
-  const path = usePathname().split('/')[1] || ''; // Default to home if no path is present
-  
   return (
     <html lang="en" suppressHydrationWarning className={instrumentSans.className}>
       <body>
